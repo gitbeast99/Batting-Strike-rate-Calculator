77234f01ba065b33800a93b8dcd975ebc73a84ef
Vinayak Nayak N <91820034+gitbeast99@users.noreply.github.com>
2023-01-02 02:21:29 +0530

    Create strikerate.py

---
+++ strikerate.py
@@ -0,0 +1,10 @@
+# Batting Strike rate calculator
+# Batting (s/r) - Avg number of runs scored per 100 balls.
+
+r = input("Enter the runs scored by batsman: ")
+r = int(r)
+b = input("Enter the number of balls faced by batsman: ")
+b = int(b)
+avg = r / b
+Sr = avg * 100
+print("The Strike rate of batsman is : ", Sr)
